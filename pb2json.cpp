eac602635088475a2291c175a832080361349603
Merlin <merlin_young@163.com>
2019-10-13 02:24:04 +0800

    build && repeated && enum

---
+++ pb2json.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <fstream>
+#include <google/protobuf/message.h>
+#include "json.hpp"
+#include "byReflection/pb2json.h"
+
+using namespace std;
+using Json =  nlohmann::json;
+using PbMessage = google::protobuf::Message;
+
+bool json2pb(google::protobuf::Message& msg, istream& is)
+{
+    Json json;
+    is>>json;
+    // auto jobj = Json::parse(str);
+    return Pb2Json::Json2Message(json, msg, true);
+}
+bool json_file2pb(PbMessage& msg, const string& json_file_path)
+{
+    ifstream ifs(json_file_path);
+    if(!ifs.is_open())
+    {
+        return false;
+    } 
+    return json2pb(msg, ifs);
+    // auto size = ifs.tellg();
+    // string str(size, '\0'); // 构造 string 为流大小
+    // ifs.seekg(0);
+    // if(!ifs.read(str.data(), size))
+    // {
+    //     return false;
+    // }
+    // auto json = Json::parse(str);
+    // return Pb2Json::Json2Message(json, msg);
+
+}
+
+bool json2pb(google::protobuf::Message& msg, const std::string& json_value)
+{
+    auto json = Json::parse(json_value);
+    return Pb2Json::Json2Message(json, msg, true);
+}
+string pb2json(const PbMessage& msg)
+{
+    Json json;
+    Pb2Json::Message2Json(msg,json);
+
+    ostringstream oss;
+    oss<<json;
+    return oss.str();
+
+}
---
+++ pb2json.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <istream>
+
+namespace google{namespace protobuf{ class Message;}}
+bool json_file2pb(google::protobuf::Message& msg, const std::string& json_file_path);
+bool json2pb(google::protobuf::Message& msg, std::istream& is);
+bool json2pb(google::protobuf::Message& msg, const std::string& json_value);
+std::string pb2json(const google::protobuf::Message& msg);
